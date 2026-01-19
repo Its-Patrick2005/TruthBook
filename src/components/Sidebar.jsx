d1cc99a89e26b02462c71133069137ed06d179ec
Its-Patrick2005 <paasare20@gmail.com>
2026-01-19 15:40:51 +0000

    one

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,7 +12,7 @@ const Sidebar = () => {
         <div className="mb-4">
           <div className="flex items-center gap-3 p-2 hover:bg-gray-100 rounded-lg cursor-pointer">
             <div className="w-10 h-10 bg-blue-600 rounded-full"></div>
-            <h1 className="font-semibold text-gray-800">UserName</h1>
+            <h1 className="font-semibold text-gray-800">User Name</h1>
           </div>
         </div>
 
